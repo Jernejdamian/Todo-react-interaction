2c71f3bc9b31cfd35209877eb23f4d10e5c1fa4f
Damian Salawa <developer20@o2.pl>
2023-11-06 20:26:38 +0100

    removeTask interaction

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ import Section from "./Section";
 import Container from "./Container";
 import { useState } from "react";
 
-const hideDone = false;
-
-const tasks = [
-  {
-    id: 1, content: "Nauczyć się programować", done: false,
-  },
-  {
-    id: 2, content: "Kupić kurs programowania", done: true,
-  },
-];
-
 function App() {
 
   const [hideDone, setHideDone] = useState(false);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone)
-  }
+  };
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1, content: "Nauczyć się programować", done: false,
+    },
+    {
+      id: 2, content: "Kupić kurs programowania", done: true,
+    },
+  ]);
+
+  const removeTask = (id)=>{
+    setTasks(tasks=>tasks.filter(task=>task.id !==id))
+  };
 
   return (
 
@@ -41,7 +43,8 @@ function App() {
         body={
           <Tasks
             tasks={tasks}
-            hideDone={hideDone} />
+            hideDone={hideDone}
+            removeTask={removeTask} />
         } />
     </Container>
   );
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,13 +1,15 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone }) => (
+const Tasks = ({ tasks, hideDone, removeTask }) => (
     <ul className="tasks">
         {
             tasks.map(task=>(
                 <li className={`task ${task.done && hideDone ? "tasks__item--hidden" : ""}`}>
                 <button className="task__button task__button--done js-done">{task.done ? "✔️" : ""}</button>
                 <span className={`task__content ${task.done ? "task__content--done" : ""}`}>{task.content}</span>
-                <button className="task__button task__button--remove js-remove">🗑</button>
+                <button 
+                onClick={()=>removeTask(task.id)}
+                className="task__button task__button--remove js-remove">🗑</button>
             </li>
             ))
         }
